d7f6b2d9c4c854ceab8fe8a620cee97e96bb7a28
Victor Bariatto Reksidler <94481634+vbreksidler@users.noreply.github.com>
2022-10-27 08:58:41 -0300

    Delete project-car-shop-victor-reksidler directory

--- project-car-shop-victor-reksidler/src/interfaces/IModel.ts
+++
@@ -1,7 +0,0 @@
-export interface IModel<T> {
-  create(obj:T):Promise<T>,
-  read(): Promise<T[]>,
-  readOne(_id:string):Promise<T | null>,
-  update(_id:string, obj:T): Promise<T | null>
-  delete(_id: string):Promise<T | null>,
-}
